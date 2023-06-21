c2eed2376d03d544a53eb6f884a8ac7658ad8020
Adnurhytia <adindanurhayati30@gmail.com>
2023-06-22 03:44:06 +0700

    Add project files.

---
+++ Discovered Environment/Halaman data prodi.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using System.Windows.Input;
+
+
+namespace Discovered_Environment
+{
+    public partial class Halaman_data_prodi : Form
+    {
+        private string stringConnection = "data Source=DESKTOP-4IT269M\\ADINDANURHAYATI;" + "database=Universitas;User ID=sa;Password=3007dinda";
+        private  SqlConnection koneksi;
+
+        private void refreshform()
+        {
+            nmp.Text = "";
+            nmp.Enabled = false;
+            btnSave.Enabled = false;
+            btnClear.Enabled = false;
+        }
+        public Halaman_data_prodi()
+        {
+            InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
+            refreshform();
+        }
+
+        private void dataGridView()
+        {
+            koneksi.Open();
+            string str = "select nama_prodi from dbo.Prodi";
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
+        }
+
+        private void Halaman_data_prodi_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dataGridView();
+            btnOpen.Enabled = false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            nmp.Enabled= true;
+            btnSave.Enabled= true;
+            btnClear.Enabled= true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            refreshform();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string nmProdi = nmp.Text;
+
+            if (nmProdi == "")
+            {
+                MessageBox.Show("Masukan Nama Prodi", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                koneksi.Open();
+                string str = "insert into dbo.Prodi (nama_prodi)" + "values(@id)";
+                SqlCommand cmd = new SqlCommand(str, koneksi);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new SqlParameter("id", nmProdi));
+                cmd.ExecuteNonQuery();
+
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView();
+                refreshform();
+            }
+        }
+        private void Halaman_data_prodi_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form1 hu = new Form1();
+            hu.Show();
+            this.Hide();
+        }
+    }
+    
+}
